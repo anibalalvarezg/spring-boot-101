9e4c8ff50732303cd4fd59c7fee22e6f55cced8c
Anibal <anibal.alvarezg@gmail.com>
2025-11-22 16:47:07 -0300

    feat: Move PropertySource annotation to new ValuesConfig class for better configuration management

--- src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,13 +2,8 @@ package com.anibal.curso.srpingboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySources({
-	@PropertySource("classpath:values.properties")
-})
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
