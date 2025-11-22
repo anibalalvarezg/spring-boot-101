f8ea5610b8459f6e5ed25d367a9d9658e25441d0
Anibal <anibal.alvarezg@gmail.com>
2025-11-22 16:40:18 -0300

    feat: Add values.properties for configuration and update SpringBoot application to use it

--- src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,8 +2,10 @@ package com.anibal.curso.srpingboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:values.properties")
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class SpringbootWebApplication {
 	}
 
 }
+ 
