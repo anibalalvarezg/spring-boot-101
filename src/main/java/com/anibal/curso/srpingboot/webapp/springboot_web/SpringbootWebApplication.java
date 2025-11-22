0548cbfa8198bb0d48cf6e266ee595ae53ddac35
Anibal <anibal.alvarezg@gmail.com>
2025-11-22 16:44:56 -0300

    fix: Correctly format PropertySource annotation for values.properties

--- src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/anibal/curso/srpingboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -3,9 +3,12 @@ package com.anibal.curso.srpingboot.webapp.springboot_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+})
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
